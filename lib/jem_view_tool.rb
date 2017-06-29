7b17fbc551177acbd67d49978747453bde504732
Jordan Hudgens <jordan@devcamp.com>
2017-06-29 16:03:00 +0000

    initial commit

---
+++ lib/jem_view_tool.rb
@@ -0,0 +1,6 @@
+require "jem_view_tool/version"
+require "jem_view_tool/renderer"
+
+module JemViewTool
+  # Your code goes here...
+end
---
+++ lib/jem_view_tool/version.rb
@@ -0,0 +1,3 @@
+module JemViewTool
+  VERSION = "0.1.0"
+end
